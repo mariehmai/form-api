25fa2f024c6cacc5d22b462c1863e7111c67922b
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-06-19 13:44:12 +0100

    add PlainTextField with length and required validation

--- form.ts
+++ form.ts
@@ -1,3 +1,5 @@
+type Field = PlainTextField
+
 class Form {
   public formId: string;
   public fields: Field[];
@@ -10,7 +12,38 @@ class Form {
   }
 }
 
-class Field {
+class BaseField {
   public fieldId: string;
   public form: Form;
+
+  constructor(public label: string) {}
+}
+
+class PlainTextField extends BaseField {
+  public minLength?: number;
+  public maxLength?: number;
+  public required: boolean;
+  public errors: string[];
+
+  constructor(public label, public value?: string) {
+    super(label)
+  }
+
+  public validate() {
+    if (this.required && !this.value) {
+      this.errors.push('value cannot be empty')
+    }
+
+    if (!this.value) {
+      return
+    }
+
+    if (this.minLength && this.value.length < this.minLength ) {
+      this.errors.push(`value must be at least ${this.minLength} characters`)
+    }
+
+    if (this.maxLength && this.value.length > this.maxLength ) {
+      this.errors.push(`value must be at most ${this.maxLength} characters`)
+    }
+  }
 }
