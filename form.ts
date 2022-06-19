07dd45ea3015923a62016a293f65f2974f49da20
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-06-19 14:54:00 +0100

    format file

--- form.ts
+++ form.ts
@@ -16,9 +16,9 @@ class Form {
     this.fields.push(field);
   }
 
-  public validate() { this.fields.forEach(f => f.validate()) }
+  public validate() { this.fields.forEach(f => f.validate()); }
 
-  public get isValid() { return this.fields.every(f => f.isValid) }
+  public get isValid() { return this.fields.every(f => f.isValid); }
 }
 
 class BaseField<TValue> {
@@ -35,16 +35,16 @@ class BaseField<TValue> {
     this.errors = [];
 
     if (this.required && !this.value) {
-      this.errors.push('value cannot be empty')
+      this.errors.push('value cannot be empty');
     }
     if (this.value) {
-      this.validateValue(this.value)
+      this.validateValue(this.value);
     }
   }
 
   public validateValue(value: TValue) { }
 
-  public get isValid() { return this.errors.length === 0 }
+  public get isValid() { return this.errors.length === 0; }
 
   public addConditional(otherField: Field, conditionalValue: Field['value']) {
     this.conditionalField = otherField;
@@ -52,7 +52,7 @@ class BaseField<TValue> {
   }
 
   public get visible() {
-    return this.conditionalField.value === this.conditionalValue
+    return this.conditionalField.value === this.conditionalValue;
   }
 }
 
@@ -62,32 +62,32 @@ class PlainTextField extends BaseField<string> {
   public regex?: RegExp;
 
   constructor(public label: string, public value?: string) {
-    super(label)
+    super(label);
   }
 
   public validateValue(value: string) {
     if (this.minLength && value.length < this.minLength) {
-      this.errors.push(`value must be at least ${this.minLength} characters`)
+      this.errors.push(`value must be at least ${this.minLength} characters`);
     }
     if (this.maxLength && value.length > this.maxLength) {
-      this.errors.push(`value must be at most ${this.maxLength} characters`)
+      this.errors.push(`value must be at most ${this.maxLength} characters`);
     }
     if (this.regex && !value.match(this.regex)) {
-      this.errors.push("invalid format")
+      this.errors.push("invalid format");
     }
   }
 }
 
 class EmailTextField extends PlainTextField {
   constructor(public label: string, public value?: string) {
-    super(label)
-    this.regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+    super(label);
+    this.regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   }
 }
 
 class BooleanField extends BaseField<boolean> {
   constructor(public label: string, public value: boolean = false) {
-    super(label)
+    super(label);
   }
 }
 
@@ -95,24 +95,24 @@ class SingleSelectField<TOption> extends BaseField<TOption> {
   public selected?: TOption;
 
   constructor(public label: string, public options: TOption[], defaultSelected?: TOption) {
-    super(label)
-    this.selected = defaultSelected
+    super(label);
+    this.selected = defaultSelected;
   }
 
   public validateValue(value: TOption) {
     if (!this.options.indexOf(value)) {
-      this.errors.push("value not in allowed choices")
+      this.errors.push("value not in allowed choices");
     }
   }
 }
 
 class FileField extends BaseField<File> {
   public maxSize?: number;
-  public allowedExtensions: string[]
+  public allowedExtensions: string[];
   public fileNameRegex?: RegExp;
 
   constructor(public label: string, public value?: File) {
-    super(label)
+    super(label);
   }
 
   public addAllowedExtension(type: string) {
@@ -121,10 +121,10 @@ class FileField extends BaseField<File> {
 
   public validateValue(value: File) {
     if (this.fileNameRegex && !value.name.match(this.fileNameRegex)) {
-      this.errors.push(`invalid file name`)
+      this.errors.push(`invalid file name`);
     }
     if (this.maxSize && value.size > this.maxSize) {
-      this.errors.push(`file size must not exceeds ${this.maxSize} bytes`)
+      this.errors.push(`file size must not exceeds ${this.maxSize} bytes`);
     }
   }
 }
